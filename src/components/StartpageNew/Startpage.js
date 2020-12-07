637e7e217c7384ba02066ff0dcb673d0d5679b06
AndyCarlsson <andreas95carlsson@gmail.com>
2020-12-07 08:55:53 +0100

    Rename test

---
+++ src/components/StartpageNew/Startpage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Startpage.scss';
+import 'normalize.css';
+
+function Startpage() {
+  return (
+    <div className='startpage-wrapper'>
+      <section className='text-container'>
+        <h1>Andreas Carlsson.</h1>
+        <p>Aspiring Developer.</p>
+        <p>Gothenburg, Sweden.</p>
+        <p>Living to create.</p>
+        <p>Minigolf (semi) pro.</p>
+      </section>
+    </div>
+  );
+}
+
+export default Startpage;
