cc33cf3b828d9f81d40acbeed4d7d4173a5b30ab
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-01-01 18:27:47 +0100

    Added ProjectsDetail page

--- src/components/Projects/ProjectsListItem.js
+++ src/components/Projects/ProjectsListItem.js
@@ -1,29 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './ProjectsListItem.scss';
 
-
-function ProjectsListItem(props){
-
-    return (
-    <div className='project-item'> 
-        <div className='image-container'>{props.item.imgURL}</div>
-        <div className='info-container'>
+function ProjectsListItem(props) {
+  return (
+    <div className='project-item'>
+      <div className='image-container'>{props.item.imgURL}</div>
+      <div className='info-container'>
         <div className='stacklist-container'>
-            {props.item.stackList.map((item, index) => (
+          {props.item.stackList.map((item, index) => (
             <p key={index}>{item}</p>
-            ))}
+          ))}
         </div>
         <h2>{props.item.title}</h2>
         <p className='item-description'>{props.item.description}</p>
-        </div>
-        <div className='button-container'>
-        <a href='/'>More info</a>
+      </div>
+      <div className='button-container'>
+        <Link to='/'>More info</Link>
         <a href={props.item.githubLink} rel='noreferrer' target='_blank'>
-            GitHub
+          GitHub
         </a>
-        </div>
-  </div>
-  )
-
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsListItem;
