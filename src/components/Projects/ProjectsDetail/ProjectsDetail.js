ee079d49e4baeaa069ec8a6e03d83854e17fb6ae
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-01-04 11:31:40 +0100

    Added the outlines for detail page

--- src/components/Projects/ProjectsDetail/ProjectsDetail.js
+++ src/components/Projects/ProjectsDetail/ProjectsDetail.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './ProjectsDetail.scss';
 
 function ProjectDetail(props) {
   const [projectObject, setProjectObject] = useState({});
@@ -14,9 +15,35 @@ function ProjectDetail(props) {
   };
 
   return (
-    <div>
-      <h1>Details</h1>
-      <h2>{projectObject.title}</h2>
+    <div className='detail-wrapper'>
+      <div className='title-container'>
+        <h1>{projectObject.title}</h1>
+        <p>{projectObject.stackList}</p>
+      </div>
+      <div className='detail-image-container'>
+        <div className='first-image-container'>
+          <p>Image 1</p>
+        </div>
+        <div className='second-image-container'>
+          <p>Image 2</p>
+        </div>
+      </div>
+      <div className='description-wrapper'>
+        <div className='description-container'>
+          <h1>Description</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laborum reiciendis
+            asperiores cumque similique dignissimos commodi. Voluptate perspiciatis accusamus
+            sapiente impedit dignissimos optio, esse facilis recusandae, autem repellat corrupti
+            enim! Aliquam obcaecati vero illum. Aspernatur eum aut sit tempora quidem, ea
+            consequuntur amet, praesentium alias incidunt saepe mollitia reiciendis nemo tenetur
+            ipsam earum vitae quos magni temporibus voluptatem corrupti accusantium.
+          </p>
+        </div>
+      </div>
+      <div className='video-container'>
+        <p>Demovideo som loopar</p>
+      </div>
     </div>
   );
 }
