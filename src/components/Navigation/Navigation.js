d8b7196a38610499a746dc58bbe9de78da2e1e73
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-01-21 13:30:55 +0100

    Removed spring from navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import NavbarSmall from './NavbarSmall';
-import { animated, useSpring } from 'react-spring';
 
 export default function Navigation() {
   const [windowDimension, setWindowDimension] = useState(null);
+
+  const isHamMenu = windowDimension <= 768;
 
   useEffect(() => {
     setWindowDimension(window.innerWidth);
@@ -14,30 +15,9 @@ export default function Navigation() {
     function handleResize() {
       setWindowDimension(window.innerWidth);
     }
-
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const isHamMenu = windowDimension <= 768;
-
-  let pathName = window.location.pathname;
-
-  // if ((pathName = '/')) {
-  //   let menu = (
-  //     <animated.div style={spring}>{isHamMenu ? <NavbarSmall /> : <Navbar />}</animated.div>
-  //   );
-  // }
-
-  const spring = useSpring({
-    from: {
-      opacity: 0,
-    },
-    to: {
-      opacity: 1,
-    },
-    delay: 3000,
-  });
-
-  return <animated.div style={spring}>{isHamMenu ? <NavbarSmall /> : <Navbar />}</animated.div>;
+  return <>{isHamMenu ? <NavbarSmall /> : <Navbar />}</>;
 }
