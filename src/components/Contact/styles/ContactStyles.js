5177030ffe2ee9bf651d3d330eb2f32bf40eeef3
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-02-25 15:35:19 +0100

    Updated contact page

--- src/components/Contact/styles/ContactStyles.js
+++ src/components/Contact/styles/ContactStyles.js
@@ -7,6 +7,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
 
   @media only screen and (max-width: 480px) {
     min-height: 100vh;
@@ -18,14 +19,17 @@ export const ContainerInner = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   gap: 2rem;
+  width: 50%;
 
   @media only screen and (max-width: 480px) {
+    min-width: 100%;
     justify-content: center;
   }
 
-  @media (max-width: 1024px) {
+  @media only screen and (max-width: 1024px) {
+    min-width: 90%;
     justify-content: center;
   }
 `;
@@ -35,22 +39,24 @@ export const ItemContainer = styled(animated.div)`
   align-items: center;
   gap: 2rem;
   padding: 1rem;
-  width: 30%;
+  width: 100%;
   border-radius: 10px;
   background-color: #dbd9d3;
 
   @media only screen and (max-width: 480px) {
-    gap: 1rem;
-    width: 90%;
+    gap: 0.2rem;
   }
 
   @media only screen and (min-width: 481px) and (max-width: 1024px) {
-    gap: 1rem;
-    width: 70%;
+    gap: 0.2rem;
   }
 `;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  @media only screen and (max-width: 480px) {
+    height: 25px;
+  }
+`;
 
 export const Details = styled.h2`
   @media only screen and (max-width: 480px) {
